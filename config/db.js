6b0cf7d5977e0544afdfb91bd9459d1b27abd768
Shweta <shweta@shwetatripathi0508@gmail.com>
2025-09-05 18:54:58 +0530

    Important chnages done

--- config/db.js
+++ config/db.js
@@ -6,8 +6,9 @@
 // });
 
 // module.exports = sequelize;
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
