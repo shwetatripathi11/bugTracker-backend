4b07ae5df1a32a4ec8def3527aad1b19d5c8d3c7
Shweta <shweta@shwetatripathi0508@gmail.com>
2025-09-05 12:38:13 +0530

    FIX: use dynamic porting

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ sequelize.sync()
     .catch(err => console.log("❌ Error syncing models: " + err));
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
